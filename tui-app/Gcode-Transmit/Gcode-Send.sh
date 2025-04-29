48b8d9e600ad7fe7029227e5cf575b551815168b
HengHao-Zhuang <henghaochuang@gmail.com>
2025-04-29 12:30:55 +0000

    feature/Update 3Dpriter tool chain

--- tui-app/Gcode-Transmit/Gcode-Send.sh
+++ tui-app/Gcode-Transmit/Gcode-Send.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-chmod +x Slic3r-1.3.0-x86_64.AppImage
-chmod +x serial
 
-./Slic3r-1.3.0-x86_64.AppImage --no-gui --load Ender-3_set.ini --output test.gcode test.stl
+
+prusa-slicer --export-gcode --load Ender-3_set.ini --output test.gcode test.stl
 ./serial
+
