b9c3f79b1598d209989c2a4406740757fbfa645a
HengHao-Zhuang <henghaochuang@gmail.com>
2025-05-01 09:56:24 +0000

    Update Gcode-Send.sh 背景執行

--- tui-app/Gcode-Transmit/Gcode-Send.sh
+++ tui-app/Gcode-Transmit/Gcode-Send.sh
@@ -1,11 +1,11 @@
-#!/bin/bash
+tui-app/Gcode-Transmit/Gcode-Send-PID.pidtui-app/Gcode-Transmit/Gcode-Send-PID.pid#!/bin/bash
 
 
 echo $$ > ~/eureka/tui-app/Gcode-Transmit/Gcode-Send-PID.pid
-prusa-slicer --export-gcode --load Ender-3_set.ini --output test.gcode test.stl
+prusa-slicer --export-gcode --load Ender-3_set.ini --output test.gcode test.stl &
 echo $! > ~/eureka/tui-app/Gcode-Transmit/Gcode-Send-PID.pid
 rm -rf test.stl
-./serial
+./serial &
 echo $! > ~/eureka/tui-app/Gcode-Transmit/Gcode-Send-PID.pid
-rm test.gcode
+rm -rf test.gcode
 
