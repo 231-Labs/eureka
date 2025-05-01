b237f0f7fe2c84202bf3ca47bf138b6a1f420b58
HengHao-Zhuang <henghaochuang@gmail.com>
2025-05-01 08:53:06 +0000

    add/Gcode-Send-PID.pid,Gcode-Stop.sh、feature/Gcode-Stop.sh

--- tui-app/Gcode-Transmit/Gcode-Send.sh
+++ tui-app/Gcode-Transmit/Gcode-Send.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-
+echo $$ > ~/eureka/tui-app/Gcode-Transmit/Gcode-Send-PID.pid
 prusa-slicer --export-gcode --load Ender-3_set.ini --output test.gcode test.stl
 rm -rf test.stl
 ./serial
