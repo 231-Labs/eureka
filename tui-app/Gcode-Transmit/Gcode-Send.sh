a933b2ddb192a2712287a1f0aefa179acdd84bef
HengHao-Zhuang <henghaochuang@gmail.com>
2025-05-01 09:34:22 +0000

    fix/Gcode-Send.sh不能取得PID問題

--- tui-app/Gcode-Transmit/Gcode-Send.sh
+++ tui-app/Gcode-Transmit/Gcode-Send.sh
@@ -3,7 +3,9 @@
 
 echo $$ > ~/eureka/tui-app/Gcode-Transmit/Gcode-Send-PID.pid
 prusa-slicer --export-gcode --load Ender-3_set.ini --output test.gcode test.stl
+echo $! > ~/eureka/tui-app/Gcode-Transmit/Gcode-Send-PID.pid
 rm -rf test.stl
 ./serial
+echo $! > ~/eureka/tui-app/Gcode-Transmit/Gcode-Send-PID.pid
 rm test.gcode
 
