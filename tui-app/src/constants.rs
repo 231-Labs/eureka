a814b227db27ada2c25b0acc582bfbb17e308db3
do0x0ob <do0x0ob@protonmail.com>
2025-05-31 18:10:27 +0800

    Update Contract Addresses

--- tui-app/src/constants.rs
+++ tui-app/src/constants.rs
@@ -5,9 +5,9 @@ pub const SCULPT_DEVNET_PACKAGE_ID: &str = "0x2571c1e364b5647e1ee17b43f9f289e5c6
 
 /// Testnet
 pub const WALRUS_COIN_TYPE: &str = "0x356a26eb9e012a68958082340d4c4116e7f55615cf27affcff209cf0ae544f59::wal::WAL";
-pub const EUREKA_TESTNET_PACKAGE_ID: &str = "0x81eac9c2b1857eb78af2453a46ea405ab3596a8101f1980bfa9b7600e576d801";
-pub const EUREKA_TESTNET_PRINTER_REGISTRY_ID: &str = "0x5b4c3ead9cc63796f0933cec5f222c4c6d8450b3190f1ba29fa8f0b1c3d1a693";
-pub const SCULPT_TESTNET_PACKAGE_ID: &str = "0x81eac9c2b1857eb78af2453a46ea405ab3596a8101f1980bfa9b7600e576d801";
+pub const EUREKA_TESTNET_PACKAGE_ID: &str = "0xfbc407f866074ade2fa71e31c8be3d05cbf096a49ced7e07dc5e22d0400275bc";
+pub const EUREKA_TESTNET_PRINTER_REGISTRY_ID: &str = "0xe2029e7661361e19e158a6916a1fb8e30d5a919cfff9b1f5688c38511236c464";
+pub const SCULPT_TESTNET_PACKAGE_ID: &str = "0xfbc407f866074ade2fa71e31c8be3d05cbf096a49ced7e07dc5e22d0400275bc";
 pub const AGGREGATOR_URL: &str = "https://walrus-agg-test.bucketprotocol.io";
 
 // Global constants
